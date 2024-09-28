b4895ad47955f3fe344f3f42ee3ad2d15b95e7d7
Catalin Mihai <75864270+qcatatv@users.noreply.github.com>
2024-09-29 02:37:09 +0300

    Add files via upload

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,5 +1,47 @@
 import React from "react";
+import "./contact.css"
 
 export const Contact = () => {
-  return <div>contact</div>;
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "YOUR_ACCESS_KEY_HERE");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+  return(
+    <div className="contactHd">
+      <h1>Contact us</h1>
+      <p>If you would like to add a comment about your order, please add it in the space below.</p>
+      <div className="contactForm">
+        <form onSubmit={onSubmit}>
+        <input placeholder="Name" type="text" name="name" required/>
+        <input placeholder="Email" type="email" name="email" required/>
+        <textarea placeholder="Subject" name="message" required></textarea>
+
+        <button type="submit">Submit Form</button>
+
+        </form>
+        <span>{result}</span>
+      </div>
+    </div>
+  )
 };
